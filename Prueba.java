524dfb9db5671f9bab2f50ac86e2d464d3315c51
Raul <raulparamo03@gmail.com>
2021-10-19 09:14:19 +0200

    more information

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 class Pruebajava {
  public static void main(String [] args){
-    System.out.println("Raúl Valle Tejón");
     System.out.println("Paramo del Sil 14/04/2003");
-    System.out.println("San Andres de Rabanedo Windows 10");	
+    System.out.println("San Andres de Rabanedo Windows 10");
+    System.out.println("Anuel AA");	
  }
 }
